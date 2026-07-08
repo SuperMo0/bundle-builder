48b28dbe1931d720622dbd6462c179b584cdfc22
super <97206982+SuperMo0@users.noreply.github.com>
2026-07-09 01:18:03 +0300

    fix: add delay to scrollIntoView for smoother animation handling

--- src/features/bundle/components/BundleAccordion/BundleAccordion.tsx
+++ src/features/bundle/components/BundleAccordion/BundleAccordion.tsx
@@ -30,7 +30,9 @@ export default function BundleAccordion({ selections, setQuantity, getSelectedCo
 
         const scrollActiveItemIntoView = (event: AnimationEvent) => {
             if (event.animationName !== 'accordion-slide-down') return
-            activeItem.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+            setTimeout(() => {
+                activeItem.scrollIntoView({ behavior: 'smooth', block: 'nearest' })
+            }, 100)
         }
 
         activeItem.addEventListener('animationend', scrollActiveItemIntoView)
